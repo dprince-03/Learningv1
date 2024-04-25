8226a94c0724cb32b08967fb11cbfca4cd9cb380
D'Prince <princeadedayo03@gmail.com>
2024-04-25 22:11:27 +0100

    update mails.js

--- Javascript/Node/mails.js
+++ Javascript/Node/mails.js
@@ -1,2 +1,24 @@
 const nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+    service : 'gmail',
+    auth : {
+        user : 'youremail@gmail.com',
+        passwd : 'yourpassword'
+    }
+});
+
+var mailoption = {
+    from : 'youremail@gmail.com',
+    to : 'adejare.adeadyo@gmail.com',
+    subject : 'Sending Email using Node.js',
+    text : 'That was easy !!!'
+};
+
+transporter.sendMail(mailoption, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        comsole.log('Email Sent : ' + info.response);
+    }
+});
